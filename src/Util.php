0123a3b526156675438d343d4b912d20586d8580
yaphet17 <yafetberhanu3@gmail.com>
2022-08-19 22:16:22 +0300

    Add generateToken method in Util class

--- src/Util.php
+++ src/Util.php
@@ -4,10 +4,12 @@ namespace Chapa;
 
 class Util{
 
+    public static function validate(){
 
-    public function validate(){
+    }
 
-
+    public static function generateToken($prefix = 'cp'){
+       return $prefix . '_' . bin2hex(random_bytes(5)) . '_' .  date('d-m-y_h-i-s');
     }
 }
 
